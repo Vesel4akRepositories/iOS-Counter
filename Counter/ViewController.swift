a1257d6d2fa005dad90c444cdb6f31e5c077813b
Vesel4ak <68870977+Vesel4akRepositories@users.noreply.github.com>
2023-12-02 18:08:14 +0300

    first commit

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterText: UILabel!
+    @IBOutlet weak var logText: UITextView!
+    
+    private var counter: Int = 0 {
+        didSet {
+            counterText.text = "\(counter)"
+            let dateString = DateFormatter.localizedString(from: Date(), dateStyle: .short, timeStyle: .medium)
+            if counter == 0 && oldValue != 0 {
+                logText.text.append("\n\(dateString): значение сброшено")
+            } else if counter > oldValue {
+                logText.text.append("\n\(dateString): значение изменено на +1")
+            } else if counter < oldValue {
+                logText.text.append("\n\(dateString): значение изменено на -1")
+            }
+            scrollToBottom()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        logText.text = "История изменений:"
+        counterText.text = "\(counter)"
     }
-
-
+    
+    @IBAction func increment() {
+        counter += 1
+    }
+    
+    @IBAction func decrement() {
+        if counter > 0 {
+            counter -= 1
+        } else {
+            let dateString = DateFormatter.localizedString(from: Date(), dateStyle: .short, timeStyle: .short)
+            logText.text.append("\n\(dateString): попытка уменьшить значение счётчика ниже 0")
+            scrollToBottom()
+        }
+    }
+    
+    @IBAction func reset() {
+        counter = 0
+    }
+    
+    
+    private func scrollToBottom() {
+        let range = NSMakeRange(logText.text.count - 1, 1)
+        logText.scrollRangeToVisible(range)
+    }
+    
 }
 
